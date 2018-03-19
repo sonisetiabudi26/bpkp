faec45f2716c3ff82f5f83c53f603a178d893f69
cipta ageung mahdiar <cipta_ageung@yahoo.co.id>
2018-03-20 00:00:08 +0700

    Update start project

---
+++ application/views/homepage.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome</title>
+<link href="<?php echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
+</head>
+<body>
+
+<div class="col-lg-12">
+	<h1 class="bg-primary"><?php echo base_url('assets/css/bootstrap.min.css'); ?></h1>
+</div>
+
+</body>
+</html>
