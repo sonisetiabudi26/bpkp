c6caa963f2701e0c01f3e659bcf125ef48cde606
Soni <soni.setiabudi@gmail.com>
2018-03-28 19:06:43 +0700

    change template dashboard

--- application/views/sertifikasi/content/header-page.php
+++ application/views/sertifikasi/content/header-page.php
@@ -1,75 +1,94 @@
-<link href="<?php echo base_url('assets/css/style-navbar.css')?>" rel="stylesheet">
-<!-- <nav class="navbar navbar-default" style="border:0px;opacity:0.9;">
+<div class="top_nav">
+  <div class="nav_menu">
+    <nav>
+      <div class="nav toggle">
+        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+      </div>
 
-   	<div class="navbar-header">
-       <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-    	    <span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-   		<a href="#" class="navbar-brand">
-			<img height="30" src="<?php //echo base_url('assets/other/media/img/logo/bpkp.png') ?>" />
-		</a>
-    </div>
-
-    <div id="navbarCollapse" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#myModal" data-toggle="modal" id="data" data-target="#modal-content"> <span class="glyphicon glyphicon-search"></span></a></li>
-        	</ul>
-        	<ul class="nav navbar-nav" style="float: right">
-        	<li >
-        	<a href="#myModal" data-toggle="modal" id="allData" data-target="#modal-content">Hai, <?php echo $username;?> </a></li>
-        	</ul>
-    </div>
-</nav> -->
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <img style="height:50px;padding-right:10px;" src="<?php echo base_url('assets/other/media/img/logo/bpkp.png') ?>" />
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        <!-- <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li> -->
-      </ul>
-      <!-- <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form> -->
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hai, <?php echo $username;?>  <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo site_url('login/logout');?>">Logout</a></li>
+      <!-- <ul class="nav navbar-nav navbar-right">
+        <li class="">
+          <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <img src="images/img.jpg" alt="">John Doe
+            <span class=" fa fa-angle-down"></span>
+          </a>
+          <ul class="dropdown-menu dropdown-usermenu pull-right">
+            <li><a href="javascript:;"> Profile</a></li>
+            <li>
+              <a href="javascript:;">
+                <span class="badge bg-red pull-right">50%</span>
+                <span>Settings</span>
+              </a>
+            </li>
+            <li><a href="javascript:;">Help</a></li>
+            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
           </ul>
         </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+
+        <li role="presentation" class="dropdown">
+          <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+            <i class="fa fa-envelope-o"></i>
+            <span class="badge bg-green">6</span>
+          </a>
+          <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+            <li>
+              <a>
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li>
+              <div class="text-center">
+                <a>
+                  <strong>See All Alerts</strong>
+                  <i class="fa fa-angle-right"></i>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul> -->
+    </nav>
+  </div>
+</div>
