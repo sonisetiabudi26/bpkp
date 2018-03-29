476e2244fbde9d24628ef758b8d2dd5f7b011ee4
Soni <soni.setiabudi@gmail.com>
2018-03-29 10:31:44 +0700

    update menu

--- application/views/sertifikasi/content/menu-page.php
+++ application/views/sertifikasi/content/menu-page.php
@@ -22,10 +22,11 @@
     <!-- sidebar menu -->
     <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
       <div class="menu_section">
-        <h3>General</h3>
+        <h3 style="color:#ADABAB !important">General</h3>
         <ul class="nav side-menu">
-          <li><a><i class="fa fa-home"></i> Home</a>
-
+            <li><a><i class="fa fa-home"></i>Home</a>
+              <li><a><i class="fa fa-home"></i>Ujian Sertifikasi</a>
+            <li><a><i class="fa fa-home"></i>Riwayat Ujian</a>
           </li>
           <!-- <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
