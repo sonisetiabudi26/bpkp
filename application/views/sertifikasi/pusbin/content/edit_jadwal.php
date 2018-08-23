dded05ce6ebed279280ad591db841f9cc287e2c4
user <user@users-MacBook-Pro.local>
2018-08-23 21:39:58 +0700

    update edit jadwal

--- application/views/sertifikasi/pusbin/content/edit_jadwal.php
+++ application/views/sertifikasi/pusbin/content/edit_jadwal.php
@@ -1,14 +1,15 @@
 <div class="col-md-12" id="response-text" ></div>
 <div class="col-md-12">
-<form onsubmit="procesForm(this, 'response-text')" action="<?php echo base_url('sertifikasi')."/pusbin/ManagementRegistrasi/edit"; ?>" method="POST" id="jadwalFrom" >
+<form onsubmit="procesFormandUpload(this, '<?php echo base_url('sertifikasi')."/pusbin/ManagementRegistrasi/edit_jadwal"; ?>')"  method="POST" id="edit_jadwalFrom" >
+	<input type="text" name='id_ujian' value="<?php echo $PK_JADWAL_UJIAN ?>" style="display:none"/>
 		<div class="form-group">
       	<label for="category">Category :</label>
-        	<input type="text" class="form-control text-primary" id="category" name="category" placeholder="Category" />
+        	<input type="text" class="form-control text-primary" value="<?php echo $CATEGORY ?>" id="category" name="category" placeholder="Category" />
     </div>
     <div class="form-group">
         <label for="startdate">Start Date :</label>
         <div class="input-group date" data-provide="datepicker" id='datetimepicker1'>
-            <input type="text" name='start_date' id='start_date' class="form-control">
+            <input type="text" name='start_date' id='start_date' value="<?php echo $START_DATE ?>" class="form-control">
             <div class="input-group-addon">
                 <span class="glyphicon glyphicon-th"></span>
             </div>
@@ -17,14 +18,14 @@
     <div class="form-group">
         <label for="enddate">End Date :</label>
         <div class="input-group date" data-provide="datepicker" id='datetimepicker2'>
-            <input type="text" name='end_date' id='end_date' class="form-control">
+            <input type="text" name='end_date' id='end_date' value="<?php echo $END_DATE ?>" class="form-control">
             <div class="input-group-addon">
                 <span class="glyphicon glyphicon-th"></span>
             </div>
           </div>
     </div>
 
-		<input id="btn-add-jadwal" type="submit" value="Submit" class="btn btn-primary" />
+		<input id="btn-edit-jadwal" type="submit" value="Ubah" class="btn btn-primary" />
 	</br>
 </form>
 </div>
